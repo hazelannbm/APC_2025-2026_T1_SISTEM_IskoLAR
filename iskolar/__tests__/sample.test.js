77eb7ae5514a4065e98fb801521b1bd2c7c99d08
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-08-24 21:45:46 +0800

    Update package.json dependencies and devDependencies

---
+++ iskolar/__tests__/sample.test.js
@@ -0,0 +1,3 @@
+test('Jest works in Jenkins', () => {
+  expect(1 + 1).toBe(2);
+});
---
+++ iskolar/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+};
