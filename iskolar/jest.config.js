cefceb21b48d9fa434f53d459175c89cf50d03a5
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-08-24 22:23:32 +0800

    Add jest-environment-jsdom dependency to package.json and update jest.config.js formatting

--- iskolar/jest.config.js
+++ iskolar/jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
+  ],
 };
