24a70f23913afa52952ea4dacc0e89b313df4a45
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-08-25 16:43:17 +0800

    chore: migrate Babel and Jest configuration to new files and update test command in package.json

---
+++ iskolar/jest.config.mjs
@@ -0,0 +1,18 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+
+export default createJestConfig(config);
