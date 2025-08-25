2851e3cf0da7f5960974b09a51e13db318ae3a97
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-08-25 16:49:39 +0800

    chore: migrate Babel configuration to babel-jest.config.js and update Jest config to reference new file

--- iskolar/jest.config.mjs
+++ iskolar/jest.config.mjs
@@ -1,18 +1,15 @@
 import nextJest from 'next/jest.js';
 
-const createJestConfig = nextJest({
-  dir: './',
-});
+const createJestConfig = nextJest({ dir: './' });
 
 const config = {
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './babel-jest.config.js' }],
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
 
 export default createJestConfig(config);
