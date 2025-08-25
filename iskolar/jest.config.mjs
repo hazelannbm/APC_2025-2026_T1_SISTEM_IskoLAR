6dbde6d53013f19305fedf314476cd7ba4021b24
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-08-25 17:26:56 +0800

    fix: add jest-dom setup to jest configuration

--- iskolar/jest.config.mjs
+++ iskolar/jest.config.mjs
@@ -4,7 +4,7 @@ const createJestConfig = nextJest({ dir: './' });
 
 const config = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // <-- add this
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // ✅ this line makes jest-dom work
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './babel-jest.config.js' }],
   },
